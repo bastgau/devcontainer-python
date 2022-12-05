4ee160f424acc95fb470ab78daaa68990279b7bd
Bastien Gautier <bastien.gautier@dcube.fr>
2022-12-05 19:35:20 +0100

    :hammer: Change git configuration

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -3,8 +3,9 @@
 sudo chgrp vscode /workspaces/app/.venv
 sudo chown vscode /workspaces/app/.venv
 
-git config --global --add safe.directory /workspaces/app/
-git config --global core.autocrlf true
+git config --global --add safe.directory /workspaces/app
+git config --global core.eol lf
+git config --global core.autocrlf false
 
 python3 -m venv /workspaces/app/.venv
 PATH="/workspaces/app/.venv/bin:$PATH"
