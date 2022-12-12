c1522b9078533f9feda338f64df21c18c039c45f
Bastien Gautier <bastien.gautier@dcube.fr>
2022-12-12 11:17:24 +0100

    :technologist: Add color and new script to check installation

---
+++ .devcontainer/check-post-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "\n${GREEN}> Display pip info/version.${ENDCOLOR}\n"
+pip --version
+
+# ADD [here] your other verification todo.
+
--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "\n${GREEN}> Configure environment.${ENDCOLOR}\n"
+
 sudo chgrp vscode /workspaces/app/.venv
 sudo chown vscode /workspaces/app/.venv
 
@@ -9,6 +15,10 @@ git config core.autocrlf false
 
 python3 -m venv /workspaces/app/.venv
 PATH="/workspaces/app/.venv/bin:$PATH"
+
+echo -e "Done.\n"
+
+echo -e "${GREEN}> Update pip tool and install dependencies.${ENDCOLOR}\n"
 
 source /workspaces/app/.venv/bin/activate
 pip install --upgrade pip
@@ -22,3 +32,5 @@ pip install --upgrade pip
 
 pip install -r /workspaces/app/requirements-dev.txt
 pip install -r /workspaces/app/requirements.txt
+
+/workspaces/app/.devcontainer/check-post-install.sh
