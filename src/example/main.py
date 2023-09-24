f463cd045d0902f6dd68f1ef67fc0c2e58bee3f0
Bastien Gautier <contact@bastiengautier.com>
2023-09-24 00:55:00 +0000

    wip

--- src/example/main.py
+++ src/example/main.py
@@ -2,6 +2,11 @@
 
 from typing import Dict
 
-toto: Dict[str, str] = {"un": "one", "deux": "two", "trois": "three", "eee": "eee"}
+toto: Dict[str, str] = {
+    "un": "one",
+    "deux": "two",
+    "trois": "three",
+    "eee": "eee",
+}
 
 print(toto)
