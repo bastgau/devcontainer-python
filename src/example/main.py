84199cc660416c616243dbdf27f7a07b9663f50e
Bastien Gautier <contact@bastiengautier.com>
2023-09-24 02:33:19 +0000

    wip

--- src/example/main.py
+++ src/example/main.py
@@ -4,14 +4,14 @@ from typing import Any, Dict
 
 import pandas as pd
 
-toto: Dict[str, str] = {
+totos: Dict[str, str] = {
     "un": "one",
     "deux": "two",
     "trois": "three",
     "eee": "eee",
 }
 
-print(toto)
+print(totos)
 
 d: Any = {"col1": [0, 1, 2, 3], "col2": pd.Series([2, 3], index=[2, 3])}
 pd.DataFrame(data=d, index=[0, 1, 2, 3])
