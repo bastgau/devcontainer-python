416350906b67ef9985cf3099f9d5bc1b3f0e0aa1
Bastien Gautier <contact@bastiengautier.com>
2023-09-25 16:28:50 +0000

    wip

--- .devcontainer/install/30-initialize-unit-testing-structure.sh
+++ .devcontainer/install/30-initialize-unit-testing-structure.sh
@@ -16,11 +16,13 @@ if [ "$pytest_enabled" != "true" ] || [ "$(ls -A "$SOURCE_PATH" | wc -l)" -eq 0 
     echo -e "\n${YELLOW}Nothing to do.${ENDCOLOR}"
 else
 
-    echo -e "\n${GREEN}> Unit testing directory structure will be created.${ENDCOLOR}\n"
+    echo -e "\n${GREEN}> Unit Testing directory structure will be created.${ENDCOLOR}"
 
     mkdir -p $UNIT_TESTING_PATH
 
     package_directories=$(ls "$SOURCE_PATH" | sort)
+
+    quantity=0
 
     for package_directory in $package_directories; do
 
@@ -42,12 +44,16 @@ def test_hello() -> None:  # pylint: disable=unused-variable
     assert app.hello() == "Hello John Doe! How are you today?"
     assert app.hello("Jane Doe") == "Hello Jane Doe! How are you today?"
 EOF
-
+            echo -e "\nUnit Testing directory for 'tests_$package_directory' created"
+            quantity=$((quantity + 1))
         fi
 
     done
 
-    echo -e "Done"
+
+    if [ "$quantity" -eq 0 ]; then
+        echo -e "\nNo Unit Testing directory has been created"
+    fi
 
 fi
 
